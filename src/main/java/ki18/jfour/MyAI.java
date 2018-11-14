2a2ee208adf4b179a5f3e70f65e42f40cce9cea3
s349989 <s349989@a001-19.cip.informatik.uni-wuerzburg.eu>
2018-11-14 14:34:49 +0100

    new version AI

--- src/main/java/ki18/jfour/MyAI.java
+++ src/main/java/ki18/jfour/MyAI.java
@@ -6,44 +6,85 @@ import java.util.Map;
 import java.util.Random;
 
 public class MyAI extends AI{
+	
+	private static Player turn;
 
 	@Override
 	public void start(Board b) {
+		turn = b.getCurrentPlayer();
 		int depth = 1;
-		while(true){
+		while(depth <= 1){
 			Move move = generateMove(b, depth);
 			setBestMove(move);
-			depth++;
+			System.out.println(depth);
+			depth++;			
 		}		
 	}
 	
 	private Move generateMove(Board b, int depth){
-		return null;
+		int score = Integer.MIN_VALUE;
+		Move move = null;
+		
+		for(Move m : b.possibleMoves()){
+			int s = minimize(b.executeMove(m), depth - 1);
+			if (s > score){
+				score = s;
+				move = m;
+			}
+		}
+		
+		return move;
 	}
 	
-	private Move maximize(Board b, int depth){
-		depth--;
+	private int maximize(Board b, int depth){
 		if(depth == 0){
-			List<Move> moves = b.possibleMoves();
-			Map<Move, Integer> scores = new HashMap<Move, Integer>();
+			return scoring(b);
+		}
+		else{
+			int score = Integer.MIN_VALUE;
+			for(Move m : b.possibleMoves()){
+				int s = minimize(b.executeMove(m), depth - 1);
+				if (s > score){
+					score = s;
+				}
+			}
+			return score;
 		}
 	}
 	
-	private Move minimize(Board b, int depth){
-		depth--;
+	private int minimize(Board b, int depth){
 		if(depth == 0){
-			
+			return scoring(b);
+		}
+		else{
+			int score = Integer.MAX_VALUE;
+			for(Move m : b.possibleMoves()){
+				int s = maximize(b.executeMove(m), depth - 1);
+				if (s < score){
+					score = s;
+				}
+			}
+			return score;
 		}
 	}
 	
 	private int scoring(Board b){
 		int score = 0;
-		Player turn = b.getCurrentPlayer();
 		
-		score = score + getVerticalPossibilities(b.getState(), turn);
-		score = score + getHorizontalPossibilities(b.getState(), turn);
-		score = score + getDiagonal45Possibilities(b.getState(), turn);
-		score = score + getDiagonal315Possibilities(b.getState(), turn);
+		if(b.getWinner() == turn){
+			return 1000;
+		}
+		if(b.getWinner() == turn.next()){
+			return -1000;
+		}
+		
+		score = score + getVerticalPossibilities(b.getState(), turn) - getVerticalPossibilities(b.getState(), turn.next());
+		score = score + getHorizontalPossibilities(b.getState(), turn) - getHorizontalPossibilities(b.getState(), turn.next());
+		score = score + getDiagonal45Possibilities(b.getState(), turn) - getDiagonal45Possibilities(b.getState(), turn.next());
+		score = score + getDiagonal315Possibilities(b.getState(), turn) - getDiagonal315Possibilities(b.getState(), turn.next());
+		
+		//System.out.println("Scoring for player " + turn + ": " + score);
+		//System.out.println(b);		
 		
 		return score;
 	}
